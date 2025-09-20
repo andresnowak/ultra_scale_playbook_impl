193881287604c1d3d3be9d7ee8858418343dd20e
Andres Nowak <andresnowak52@gmail.com>
2025-09-20 12:26:53 +0200

    started learning about elementary operations in parallel programming for distributed workers

---
+++ README.md
@@ -0,0 +1,23 @@
+# Parallel programming
+
+## Elementary operations
+
+### Broadcast
+
+Here the idea is we have a root node and this root node will broadcast a piece of memory (here a tensor) to all available processes (gpus here)
+
+### Reduce
+
+In reduce we want to combine the data on each process through a function $f()$, so in this case somebody has to run this function $f()$ (there is no free-flying magic process which runs it), so in the case of reduce we will have root node that will receive the data of all processes and run the reduce operation with also its own data and only the root node will have this information
+
+### All Reduce
+
+In all reduce now instead we can have our processes in a Tree or ring formation, in the ring structure each rank (now will call it this instead of process) has a "next" and "previous" neighbor. In the ring formation each rank will send its data to the next neighbor and will also recive the data of its previous neignor and the rank will reduce the information it gets from its previous neigbhor and then send this data again to the next neighbor and after $P-1$ steps ($P$ being the size of the world), every rank will have the complete reduction of the data of all ranks
+
+### Gather
+For gather we can say this is similar to broadcast, but the difference is now we say that each rank has a piece of data and we want to gather all of that data in this case in one rank (our root). So here we create only a container list of data (here of tensors) on the root rank and then we gather all of the data from the whole world in the root rank
+
+### All Gather
+And for all gather the difference is that now instead al ranks will have a container list and everyone will gather the information of all ranks
+
+(Here in NCCL all gather will also behave in ring structure from what I understand)
--- parallel_programming.py
+++ parallel_programming.py
@@ -1,9 +1,11 @@
 import torch
 import torch.distributed as dist
 
+
 def init_process():
-    dist.init_process_group(backend='nccl')
+    dist.init_process_group(backend="nccl")
     torch.cuda.set_device(dist.get_rank())
+
 
 def example_broadcast():
     if dist.get_rank() == 0:
@@ -14,3 +16,53 @@ def example_broadcast():
     print(f"Before broadcast on rank {dist.get_rank()}: {tensor}")
     dist.broadcast(tensor, src=0)
     print(f"After broadcast on rank {dist.get_rank()}: {tensor}")
+
+
+def example_reduce():
+    tensor = torch.tensor([dist.get_rank() + 1] * 5, dtype=torch.float32).cuda()
+
+    print(f"Before reduce on rank {dist.get_rank()}: {tensor}")
+    dist.reduce(tensor, dst=0, op=dist.ReduceOp.SUM)
+    print(f"After reduce on rank {dist.get_rank()}: {tensor}")
+
+
+def example_all_reduce():
+    tensor = torch.tensor([dist.get_rank() + 1] * 5, dtype=torch.float32).cuda()
+
+    print(f"Before all reduce on rank {dist.get_rank()}: {tensor}")
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
+    print(f"After all reduce on rank {dist.get_rank()}: {tensor}")
+
+
+def example_gather():
+    tensor = torch.tensor([dist.get_rank() + 1] * 5, dtype=torch.float32).cuda()
+
+    if dist.get_rank() == 0:
+        gather_list = [
+            torch.zeros(5, dtype=torch.float32).cuda()
+            for _ in range(dist.get_world_size())
+        ]
+    else:
+        gather_list = None
+
+    print(f"Before gather on rank {dist.get_rank()}: {gather_list}")
+    dist.gather(tensor, gather_list, dst=0)
+    print(f"After gather on rank {dist.get_rank()}: {gather_list}")
+
+
+def example_all_gather():
+    tensor = torch.tensor([dist.get_rank() + 1] * 5, dtype=torch.float32).cuda()
+
+    gather_list = [
+        torch.zeros(5, dtype=torch.float32).cuda() for _ in range(dist.get_world_size())
+    ]
+
+    print(f"Before gather on rank {dist.get_rank()}: {gather_list}")
+    dist.all_gather(gather_list, tensor)
+    print(f"After gather on rank {dist.get_rank()}: {gather_list}")
+
+
+init_process()
+example_all_gather()
+
+dist.destroy_process_group()
